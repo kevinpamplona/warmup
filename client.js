4e46e6454462c20eb8a8f135997fb6214c0d70f5
Kevin Pamplona <kevinpamplona@gmail.com>
2014-02-18 01:49:41 -0800

    added js

---
+++ client.js
@@ -0,0 +1,11 @@
+function json_request(page, dict, success, failure) {
+    $.ajax({
+        type: 'POST',
+        url: page,
+        data: JSON.stringify(dict),
+        contentType: "application/json",
+        dataType: "json",
+        success: success,
+        error: failure
+    });
+}
